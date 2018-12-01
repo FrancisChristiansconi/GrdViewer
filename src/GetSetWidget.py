899590e9078918e266ac9f5d6339766f2820ef70
FrancisChristiansconi <christian.francesconi.fr@gmail.com>
2018-12-02 00:40:50 +0100

    cleaning up files

--- src/GetSetWidget.py
+++
@@ -1,9 +0,0 @@
-
-from PyQt5.QtWidgets import QDialog, QPushButton, QSizePolicy, QAction, qApp, QLineEdit, QDoubleValidator, QHBoxLayout, QVBoxLayout
-
-
-class GetSetWidget(QDialog):
-
-    def __init__(self, strLabel, strDefVal):
-        # Call to generic constructor
-        super().__init__()

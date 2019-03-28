75580202617521fb7eb562fbda7418796c3ee291
FrancisChristiansconi <christian.francesconi.fr@gmail.com>
2019-03-28 16:13:52 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='patternviewer',
+      version='1.1.3',
+      description='executable grd and pat file manager with GUI',
+      url='',
+      author='C. Francesconi',
+      author_email='christian.francesconi@ses.com',
+      license='None',
+      packages=['patternviewer'],
+      zip_safe=False)
+      

6b58fef2ba91e9e714d7358a63dba10c72cb8c4a
FrancisChristiansconi <christian.francesconi.fr@gmail.com>
2019-08-21 17:23:06 +0200

    creation of virtual environment for cartopy adaption

---
+++ grdviewer-cartopy/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\cfrance\Dev\Python\GrdViewer\grdviewer-cartopy"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(grdviewer-cartopy) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END

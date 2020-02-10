3dba7e6bae2a995e0c8a42cc3d04ce74f09869f7
Christian Francesconi <Christian.Francesconi@ses.com>
2020-02-10 16:48:41 +0100

    Better script to distribute executable file

--- distrib.bat
+++ distrib.bat
@@ -1,12 +1,12 @@
-REM checkout master branch as it should be the only one to be published
-git checkout master
+REM checkout %1 branch as it should be the only one to be published
+git checkout %1
 
-REM Create standalone executable
+REM REM Create standalone executable
 python -m PyInstaller --onefile --icon=grdviewer.ico --clean grdviewer.py
 
-REM Backup last version of executable
+REM REM Backup last version of executable
 mkdir P:\Antenna_models\GrdViewer\old
 copy P:\Antenna_models\GrdViewer\grdviewer.exe P:\Antenna_models\GrdViewer\old\grdviewer.exe
 
-REM Copy executable to Payload EU
+REM REM Copy executable to Payload EU
 copy .\dist\grdviewer.exe P:\Antenna_models\GrdViewer\

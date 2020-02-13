12b7078fe3909f5a418172eed8d2e692e3548ebf
Christian Francesconi <Christian.Francesconi@ses.com>
2020-02-13 11:12:23 +0100

    Update of README and distribution script

--- distrib.bat
+++ distrib.bat
@@ -6,8 +6,8 @@ python -m PyInstaller --onefile --icon=grdviewer.ico --clean grdviewer.py
 
 REM REM Backup last version of executable
 mkdir P:\Antenna_models\GrdViewer\old
-copy P:\Antenna_models\GrdViewer\grdviewer.exe P:\Antenna_models\GrdViewer\old\grdviewer.exe
+copy P:\Antenna_models\GrdViewer\grdviewer-%1.exe P:\Antenna_models\GrdViewer\old\grdviewer-%1.exe
 
 REM REM Copy executable to Payload EU
-copy .\dist\grdviewer.exe P:\Antenna_models\GrdViewer\
-copy .\README.md P:\Antenna_models\GrdViewer\
+copy .\dist\grdviewer.exe P:\Antenna_models\GrdViewer\grdviewer-%1.exe
+copy .\README.md P:\Antenna_models\GrdViewer\README-%1.md
